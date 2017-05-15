3f555ca871fc262e73896dc36c0660c925dc07dd
Airam Navas <alu0100618426@ull.edu.es>
2017-05-15 01:35:35 +0100

    Ejemplo boton con imagen

---
+++ src/ImageButton/app/src/main/java/com/example/airam/imagebutton/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.airam.imagebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTextView1((TextView) findViewById(R.id.textView1));
+    }
+
+    public void llamar(View view){
+        getTextView1().setText("Llamando...");
+    }
+
+    public TextView getTextView1() {
+        return textView1;
+    }
+
+    public void setTextView1(TextView textView1) {
+        this.textView1 = textView1;
+    }
+}
